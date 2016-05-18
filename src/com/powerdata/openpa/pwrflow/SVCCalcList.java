ac63cb6c5916a72b7c1a0156a2511ad78adc3602
Chris Mosier <chris@powerdata.com>
2016-05-18 14:11:56 -0700

    add 2016 bsd-3 clause licence header

--- src/com/powerdata/openpa/pwrflow/SVCCalcList.java
+++ src/com/powerdata/openpa/pwrflow/SVCCalcList.java
@@ -1,5 +1,11 @@
 package com.powerdata.openpa.pwrflow;
-import java.util.AbstractList;
+/*
+ * Copyright (c) 2016, PowerData Corporation, Incremental Systems Corporation
+ * All rights reserved.
+ * Licensed under the BSD-3 Clause License.
+ * See full license at https://powerdata.github.io/openpa/LICENSE.md
+ */
+
 import com.powerdata.openpa.Bus;
 import com.powerdata.openpa.BusList;
 import com.powerdata.openpa.BusRefIndex;
